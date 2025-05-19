9090342f1f0cc4cb7459ed952391b3f7dd8b2783
melau-eddy <eddymutugik@gmail.com>
2025-05-19 10:39:26 +0300

    freeze.txt requirements

--- gmo/settings.py
+++ gmo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ei9s=@j)=ielp2(=_i3xoye_*p#i9sld8_--wxjf+@3gbh^nrl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
